9f0f5e50c6883585a1b6eb0565f921856670f3e7
asfuz <ubuntusamandar@gmail.com>
2023-11-25 21:54:30 +0500

    api

--- app/User.php
+++ app/User.php
@@ -43,7 +43,7 @@ class User extends DB
     public function generate_garden($garden, $area){
         // $garden->user_phone = $this->phone;
 
-        $sql = "INSERT INTO `gardens` (`location`, `area`, `user_phone`, `status`, `description`, `price_for_kg`, `pictures`, `filters`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        $sql = "INSERT INTO `gardens` (`location`, `title`, `area`, `count`, `user_phone`, `status`, `description`, `price_for_kg`, `pictures`, `filters`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
         $stmt = self::$connection->prepare($sql);
         
@@ -54,24 +54,24 @@ class User extends DB
         $garden->price_for_kg = $garden->price_for_kg[rand(0, count($garden->price_for_kg) - 1)];
         $garden->filters = $garden->filters[rand(0, count($garden->filters) - 1)];
         $garden->title = $garden->title[rand(0, count($garden->title) - 1)];
-        $pictures = array('apple', 'banana', 'orange', 'grape');
+        $garden->count = rand(1, 100);
         
-
         // Sample array of fruit names
-        $garden->pictures = APP_URL . "/pictures" . '/' . $pictures[rand(0, count($pictures) - 1)] . 'jpg';
+        $garden->pictures = "https://m3574.myxvest.ru/bogbor". "/pictures" . '/' . $garden->title . '.jpg';
 
         $area = json_encode($area);
 
         // Bind parameters
-        $stmt->bind_param("ssssssss", $garden->location, $area, $garden->user_phone, $garden->status, $garden->description, $garden->price_for_kg, $garden->pictures, $garden->filters);
+        $stmt->bind_param("sssissssss", $garden->location, $garden->title, $area, $garden->count, $garden->user_phone, $garden->status, $garden->description, $garden->price_for_kg, $garden->pictures, $garden->filters);
   
         // Execute the statement
         $stmt->execute();
         return self::$connection->insert_id;
     }
 
-    public function get_garden(){
-
+    public function get_garden($id){
+        $sql = "SELECT * FROM `gardens` WHERE `id`=$id";
+        return self::$connection->query($sql)->fetch_all(MYSQLI_ASSOC)[0];
     }
     public function get_gardens(){
         $sql = "SELECT * FROM `gardens`";
--- index.php
+++ index.php
@@ -6,7 +6,6 @@ error_reporting(E_ALL);
 
 require_once 'loader.php';
 
-
 use app\DB;
 use app\User;
 
@@ -24,43 +23,43 @@ $user_phone = $jsonData->user_phone ?? ADMIN_PHONE;
 $user = new User($user_phone);
 $locations = json_decode(file_get_contents('locations.json'));
 
-
 // Process the request based on the endpoint
-foreach ($segments as $segment) {
-    switch ($segment) {
-        case 'create-garden':
+$segment = $segments[0];
+switch ($segment) {
+    case 'create-garden':
+        $garden = $jsonData->garden;
+        $garden = $user->add_garden($garden);
+        $response = $garden;
+        break;
+    case 'get-gardens':
+        $gardens = $user->get_gardens();
+        $response = $gardens;
+        break;
+    case 'get-garden':
+        $id = $segments[1];
+        $garden = $user->get_garden($id);
+        $response = $garden;
+        break;
+    case 'generate':
+        foreach ($locations as $area) {
             $garden = $jsonData->garden;
-            $garden = $user->add_garden($garden);
-            $response = $garden;
-            break;
-        case 'get-gardens':
-            $gardens = $user->get_gardens();
-            $response = $gardens;
-            break;
-        case 'debug':
-            foreach ($locations as $area) {
-                $garden = $jsonData->garden;
 
-                $garden->location = array("Marg'ilon", "Farg'ona", "Toshkent");
-                $garden->user_phone = array('+99890782248', '+998901234567', '+998936587451');
-                $garden->status = array('published', 'verified');
-                $garden->description = array('pishgan, mazzali', '1 haftada pishadi', 'lorem bor mashetta', 'judayam oz qoldi, atiga 4 kilo', '1 oyda pishadi');
-                $garden->price_for_kg = array("100ming so'm", "10000 so'm");
-                $garden->filters = array(1, 2, 3, 4,);
-                $garden->title = array('apple', 'banana', 'orange', 'grape');
-        
-                $garden = $user->generate_garden($garden, $area);
-            }
-            $response = $user->get_gardens();
-            break;
+            $garden->location = array("Marg'ilon", "Farg'ona", "Toshkent");
+            $garden->user_phone = array('+99890782248', '+998901234567', '+998936587451');
+            $garden->status = array('published', 'verified');
+            $garden->description = array('pishgan, mazzali', '1 haftada pishadi', 'lorem bor mashetta', 'judayam oz qoldi, atiga 4 kilo', '1 oyda pishadi');
+            $garden->price_for_kg = array("100ming so'm", "10000 so'm");
+            $garden->filters = array(1, 2, 3, 4,);
+            $garden->title = array('olma', 'banan', 'mandarin', 'uzum');
 
-
-        default:
-            $response = array('error' => 'Invalid endpoint');
-            break;
-    }
+            $garden = $user->generate_garden($garden, $area);
+        }
+        $response = $user->get_gardens();
+        break;
+    default:
+        $response = array('error' => 'Invalid endpoint');
+        break;
 }
-
 
 // Set headers to indicate JSON content type
 header('Content-Type: application/json');
